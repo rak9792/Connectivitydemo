63318131927f38637066005201508e8159650b80
rakshit <rv09071992@gmail.com>
2017-05-12 15:58:58 +0530

    trial

---
+++ src/JdbcExample.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+public class JdbcExample {
+	public static void main(String arg[])
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/db","root","root");
+			Statement st=con.createStatement();
+			ResultSet rs=st.executeQuery("select * from sample");
+			while(rs.next())
+			{
+				System.out.println(rs.getString(1));
+			}
+			con.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
